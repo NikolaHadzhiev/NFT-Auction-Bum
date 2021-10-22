c73f9411c1b834977776ba08d8634c5ef559481b
Nikola Hadzhiev <nikihad2012@abv.bg>
2021-10-22 18:49:13 +0300

    feat: Created user component with Avatar, username and info

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,12 @@
 import Header from "../header/Header.jsx"
 import Footer from "../footer/footer.jsx"
-import Avatar from "../avatar/Avatar.jsx"
+import User from "../user/User.jsx"
 
 export default function HomeComponent() {
   return (
     <>
       <Header />
-      <Avatar />
+      <User name={'terika77'} info={'134 Items'} avatar={'/images/avatar.png'} verified={true} />
       <Footer />
     </>
   );
---
+++ src/components/user/User.jsx
@@ -0,0 +1,15 @@
+import styles from './User.module.scss'
+import classNames from "classnames";
+import Avatar from "../avatar/Avatar.jsx"
+
+export default function User({ name = "", info = "", avatar = "", size = 55, verified = false }) {
+    return (
+        <div className={classNames(styles.user)}>
+            <Avatar url={avatar} size={size} verified={verified}></Avatar>
+            <div className={classNames(styles.metaInfo)}>
+                <p className={classNames(styles.name)}>{name}</p>
+                <p className={classNames(styles.info)}>{info}</p>
+            </div>
+        </div>
+    )
+}
