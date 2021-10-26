499cedf876c3dcfba29290a74ea71955b5ac9fd5
Nikola Hadzhiev <nikihad2012@abv.bg>
2021-10-26 14:53:21 +0300

    refactor: Used object destructuring in Card component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -9,24 +9,24 @@ import styles from './Card.module.scss'
 import classNames from "classnames";
 import millify from "millify";
 
-function Card(props) {
+function Card({name, likes, mediaUrl, user, price, currency}) {
     return (
         <MuiCard className={classNames(styles.card)}>
-            <CardHeader sx={{ padding: '1.0625rem 1rem 0.4375rem 1rem' }} avatar={<Avatar url={props.user.avatarUrl} size={45} verified={props.user.verified} />} />
+            <CardHeader sx={{ padding: '1.0625rem 1rem 0.4375rem 1rem' }} avatar={<Avatar url={user.avatarUrl} size={45} verified={user.verified} />} />
             <CardMedia className={classNames(styles.media)}
                 component="img"
-                image={props.mediaUrl}
+                image={mediaUrl}
                 alt="NFT Auction"
             />
             <CardActions disableSpacing sx={{ padding: '0.25rem 0.9375rem 1.375rem 0.9375rem' }}>
                 <div className={classNames(styles.cardAction)}>
-                    <span className={classNames(styles.title)}>{props.name}</span>
-                    {props.price.includes(".") ?
-                        <span className={classNames(styles.price)}>~{props.price} {props.currency}</span> :
-                        <span className={classNames(styles.price)}>{props.price} {props.currency}</span>
+                    <span className={classNames(styles.title)}>{name}</span>
+                    {price.includes(".") ?
+                        <span className={classNames(styles.price)}>~{price} {currency}</span> :
+                        <span className={classNames(styles.price)}>{price} {currency}</span>
                     }
                 </div>
-                <Chip className={classNames(styles.likes)} icon={<FavoriteIcon className={classNames(styles.icon)} />} label={millify(props.likes, { lowercase: true })} variant="outlined" />
+                <Chip className={classNames(styles.likes)} icon={<FavoriteIcon className={classNames(styles.icon)} />} label={millify(likes, { lowercase: true })} variant="outlined" />
             </CardActions>
         </MuiCard>
     )
