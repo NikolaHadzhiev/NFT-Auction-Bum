3258b69bd1663c51c5d2036411a9028bb68bf25c
Nikola Hadzhiev <nikihad2012@abv.bg>
2021-11-05 17:28:14 +0200

    feat: Added Featured component that uses Grid for positioning images

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,43 @@
+import Container from "@mui/material/Container";
+import styles from './Featured.module.scss'
+import classNames from "classnames";
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import { useRouter } from 'next/router'
+
+export default function Featured({ items = [] }) {
+    const router = useRouter()
+
+    const handleClick = (href) => {
+        router.push(href)
+      }
+
+    return (
+        <Container maxWidth="xl" disableGutters={true}>
+            <ImageList className={classNames(styles.grid_wrapper)} gap={20}>
+                {items.map((item, index) => (
+                    index == 0 ? <ImageListItem className={classNames(styles.grid_first_image)} key={item.image}>
+                        <img
+                            src={`${item.img}`}
+                            srcSet={`${item.image}`}
+                            alt={item.title}
+                            loading="lazy"
+                            className={classNames(styles.grid_image)}
+                            onClick={() => handleClick(item.href)}
+                        />
+                    </ImageListItem> :
+                        <ImageListItem key={item.image}>
+                            <img
+                                src={`${item.img}`}
+                                srcSet={`${item.image}`}
+                                alt={item.title}
+                                loading="lazy"
+                                className={classNames(styles.grid_image)}
+                                onClick={() => handleClick(item.href)}
+                            />
+                        </ImageListItem>
+                ))}
+            </ImageList>
+        </Container>
+    )
+}
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,6 +4,7 @@ import Trending from "../trending/Trending.jsx"
 import Card from "../card/Card.jsx"
 import Auctions from "../auctions/Auctions.jsx"
 import How from "../how/How.jsx"
+import Featured from "../featured/Featured.jsx"
 
 let cards = [
   {
@@ -42,28 +43,75 @@ let cards = [
 
 let component = {
   title: "How it works",
-    description: `Discover, collect, and sell extraordinary NFTs
+  description: `Discover, collect, and sell extraordinary NFTs
       on the world's first & largest NFT marketplace. There are  three things you'll need in place to open your account and start buying or selling NFTs on BUM.`,
-    items: [
-      {
-        title: "Digital Currency",
-        description:
-          "You can get ETH, the digital currency that fuels transactions on the Ethereum blockchain, from a digital currency exchange",
-      },
-      {
-        title: "Crypto Wallet",
-        description:
-          "A crypto wallet, such as MetaMask, stores your ETH and processes transactions on the Ethereum blockchain.",
-      },
-      {
-        title: "BUM.",
-        description:
-          "Let's connect your wallet to BUM, edit your profile, and begin interacting in the space.",
-      },
-    ],
-    link: "https://app.boom.dev/"
+  items: [
+    {
+      title: "Digital Currency",
+      description:
+        "You can get ETH, the digital currency that fuels transactions on the Ethereum blockchain, from a digital currency exchange",
+    },
+    {
+      title: "Crypto Wallet",
+      description:
+        "A crypto wallet, such as MetaMask, stores your ETH and processes transactions on the Ethereum blockchain.",
+    },
+    {
+      title: "BUM.",
+      description:
+        "Let's connect your wallet to BUM, edit your profile, and begin interacting in the space.",
+    },
+  ],
+  link: "https://app.boom.dev/"
 
 }
+
+let items = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=500&h=500",
+    title: "Breakfast",
+    rows: 2,
+    cols: 3,
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1551782450-a2132b4ba21d?w=500&h=500",
+    title: "Burger",
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1522770179533-24471fcdba45?w=500&h=500",
+    title: "Camera",
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c?w=500&h=500",
+    title: "Coffee",
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1533827432537-70133748f5c8?w=500&h=500",
+    title: "Hats",
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62?w=500&h=500",
+    title: "Honey",
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6?w=500&h=500",
+    title: "Basketball",
+    href: "/about",
+  },
+]
 
 export default function HomeComponent() {
   return (
@@ -72,8 +120,8 @@ export default function HomeComponent() {
       {/* <Card name="Clock" likes={1100} mediaUrl="/images/nft.jpg" user={{avatarUrl: "/images/avatar.png", verified: true}} price= "12.2" currency="ETH" timeLeft={3.6e6}/> */}
       {/* <Auctions cards={cards}/> */}
       <br />
-      <br />
-      <How title={component.title} description={component.description} items={component.items} link={component.link} />
+      <Featured items={items}/>
+      {/* <How title={component.title} description={component.description} items={component.items} link={component.link} /> */}
       <Footer />
     </>
   );
