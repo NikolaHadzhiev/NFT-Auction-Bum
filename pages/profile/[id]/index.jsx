0b4b0bacd4e00c08b22fef5adc83c456cc3243fb
Nikola Hadzhiev <nikihad2012@abv.bg>
2021-12-07 16:46:55 +0200

    feat: Implemented ProfileCollection component and created Profile page

--- pages/profile/[id]/index.jsx
+++ pages/profile/[id]/index.jsx
@@ -1,16 +1,60 @@
 import Header from "../../../src/components/header/Header.jsx"
 import ProfileHero from "../../../src/components/profile/ProfileHero.jsx"
 import ProfileUser from "../../../src/components/profile/ProfileUser.jsx"
-import ProfileCollectionFilters from '../../../src/components/profile/ProfileCollectionFilters.jsx';
+import ProfileCollection from "../../../src/components/profile/ProfileCollection.jsx"
+import dataProfile from "../../../data/profile.json"
 import Footer from "../../../src/components/footer/Footer.jsx"
 
 export default function Profile() {
+    const filters = {
+        "sort": [
+            {
+                "label": "Date (Ascending)",
+                "value": 1
+            },
+            {
+                "label": "Date (Descending)",
+                "value": 2
+            },
+            {
+                "label": "Name (Ascending)",
+                "value": 3
+            },
+            {
+                "label": "Name (Descending)",
+                "value": 4
+            },
+            {
+                "label": "Price (Ascending)",
+                "value": 5
+            },
+            {
+                "label": "Price (Descending)",
+                "value": 6
+            }
+        ],
+        "price": [
+            {
+                "label": "0 - 0.01 ETH",
+                "value": 7
+            },
+            {
+                "label": "0.01 - 0.04 ETH",
+                "value": 8
+            },
+            {
+                "label": "0.04 - 0.07 ETH",
+                "value": 9
+            }
+        ]
+    }
+
     return (
         <>
             <Header />
-            <ProfileHero image={"/images/nft.jpg"} />
-            <ProfileUser name={"Fon40"} info={"Tarikata"} avatar={"/images/avatar.png"} verified={true} />
-            <ProfileCollectionFilters filters={{"sort":[{"label":"Name (Ascending)","value":1},{"label":"Name (Descending)","value":2},{"label":"Price (Ascending)","value":4},{"label":"Price (Descending)","value":5}],"price":[{"label":"0.3 - 0.5 ETH","value":6},{"label":"0.5 - 2 ETH","value":7},{"label":"2- 3 ETH","value":8}]}} />
+            <ProfileHero image={dataProfile?.avatar?.backgroundUrl} />
+            <ProfileUser name={dataProfile?.username} info={dataProfile?.info} avatar={dataProfile?.avatar?.url} verified={dataProfile?.verified} />
+            <ProfileCollection filters={filters} user={dataProfile} items={dataProfile?.nfts} />
             <Footer />
         </>
     )
