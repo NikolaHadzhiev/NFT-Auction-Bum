0658c74872f8a90d394f39f2489cf66c60f814fd
Nikola Hadzhiev <nikihad2012@abv.bg>
2021-12-08 15:44:36 +0200

    feat: Implemented ActivityListItem

--- pages/activity/index.jsx
+++ pages/activity/index.jsx
@@ -1,12 +1,32 @@
 import Header from "../../src/components/header/Header.jsx"
 import ActivityFilters from "../../src/components/activity/ActivityFilters.jsx";
+import ActivityListItem from "../../src/components/activity/ActivityListItem.jsx";
 import Footer from "../../src/components/footer/Footer.jsx"
 
 export default function Index() {
+    const data = {
+        "created_at":"2021-10-22T08:29:23.382Z",
+        "user":{
+           "avatarUrl":"/images/avatar.png",
+           "verified":"true",
+           "name":"Cupcat NFT"
+        },
+        "nft":{
+           "name":"Dog Bone",
+           "user":{
+              "name":"Antonio Banderas",
+              "avatarUrl":"String",
+              "verified":"Boolean"
+           }
+        },
+        "type":"like"
+       }
+
     return (
         <>
             <Header />
             <ActivityFilters filters={{ "sort": [{ "label": "Name (Ascending)", "value": 1 }, { "label": "Name (Descending)", "value": 2 }], "type": [{ "label": "Liked", "value": 3 }, { "label": "Bought", "value": 4 }] }} />
+            <ActivityListItem {...data}/>
             <Footer />
         </>
     )
---
+++ src/components/activity/ActivityListItem.jsx
@@ -0,0 +1,17 @@
+import { formatDistance, parseISO } from "date-fns"
+import Avatar from '../avatar/Avatar.jsx'
+import Link from '../link/Link.jsx'
+import classNames from "classnames";
+import styles from "./ActivityListItem.module.scss"
+
+export default function ActivityItem({ user, created_at, nft, type = 'like' }) {
+    return (
+        <div className={classNames(styles["activity-item-container"])}>
+            <Avatar size={70} url={user.avatarUrl} verified={user.verified} />
+            <div className={classNames(styles["activity-item-para"])}>
+                <p><strong>{user.name}</strong> {type == 'like' ? 'liked' : 'bought'} <Link href="" color="secondary">"{nft.name}"</Link> by <Link href="" color="secondary">{nft.user.name}</Link></p>
+                <p className={classNames(styles.date)}>{formatDistance(parseISO(created_at), Date.now())} ago</p>
+            </div>
+        </div>
+    )
+}
