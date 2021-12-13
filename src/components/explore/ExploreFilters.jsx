0630062281177b76943a1a3809bb4b2d5fd806a8
Nikola Hadzhiev <nikihad2012@abv.bg>
2021-12-13 12:55:52 +0200

    feat: Implemented ExploreFilters component

---
+++ src/components/explore/ExploreFilters.jsx
@@ -0,0 +1,54 @@
+import classNames from "classnames";
+import styles from "./ExploreFilters.module.scss"
+import { FormControl, Select, InputLabel, MenuItem, Stack, TextField, InputAdornment} from "@mui/material";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from '@mui/icons-material/Search';
+import { useState } from "react";
+
+export default function ExploreFilters({ filters }) {
+    const [sortValue, setSortValue] = useState("")
+    const [sortPriceValue, setSortPrice] = useState("")
+
+    const handleSort = (event) => {
+        setSortValue(event.target.value)
+    }
+
+    const handleSortType = (event) => {
+        setSortPrice(event.target.value)
+    }
+
+    return (
+        <div className={classNames(styles['explore-filters'])}>
+            <Stack spacing={1} direction="row" alignItems="center" justifyContent="flex-end">
+                <FormControl className={classNames(styles['select-buttons'])}>
+                    <InputLabel id="sort-by-label">Sort by</InputLabel>
+                    <Select
+                        IconComponent={KeyboardArrowDownIcon}
+                        labelId="select-sort-by-label"
+                        value={sortValue}
+                        onChange={handleSort}>
+                        {filters.sort.map(filter => {
+                             return <MenuItem value={filter.value} key={filter.value}>{filter.label}</MenuItem>
+                        })}
+                    </Select>
+                </FormControl>
+                <FormControl className={classNames(styles['select-buttons'])}>
+                    <InputLabel id="price-range-label">Price range</InputLabel>
+                    <Select
+                        IconComponent={KeyboardArrowDownIcon}
+                        labelId="select-price-range-label"
+                        value={sortPriceValue}
+                        onChange={handleSortType}>
+                        {filters.price.map(filter => {
+                             return <MenuItem value={filter.value} key={filter.value}>{filter.label}</MenuItem>
+                        })}
+                    </Select>
+                </FormControl>
+                <FormControl className={classNames(styles['search'])}>
+                    <TextField className={classNames(styles['text-field'])} InputProps={{startAdornment: (<InputAdornment position="start" color="secondary" variant="standard" ><SearchIcon sx={{ color: "#E1E1FC" }} /></InputAdornment>)}} />
+                </FormControl>
+            </Stack>
+        </div>
+    )
+}
+
