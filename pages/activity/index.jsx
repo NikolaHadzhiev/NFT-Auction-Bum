1c6c968394e866a38e5d29157ebfc4feec9fc0b8
Nikola Hadzhiev <nikihad2012@abv.bg>
2021-12-13 14:44:33 +0200

    feat: Implemented Activity page

--- pages/activity/index.jsx
+++ pages/activity/index.jsx
@@ -1,51 +1,17 @@
+import dataActivity from "../../data/activity.json"
 import Header from "../../src/components/header/Header.jsx"
+import Hero from "../../src/components/hero/Hero.jsx"
 import ActivityFilters from "../../src/components/activity/ActivityFilters.jsx";
 import ActivityList from "../../src/components/activity/ActivityList.jsx";
 import Footer from "../../src/components/footer/Footer.jsx"
 
-export default function Index() {
-    const data = [
-        {
-          "created_at": "2021-10-22T08:29:23.382Z",
-          "user": {
-            "avatarUrl": "/images/avatar.png",
-            "verified": "true",
-            "name": "sth"
-          },
-          "nft": {
-            "name": "String",
-            "user": {
-              "name": "String",
-              "avatarUrl": "String",
-              "verified": "Boolean"
-            }
-          },
-          "type": "buy"
-        },  
-        {
-          "created_at": "2021-10-22T08:29:23.382Z",
-          "user": {
-            "avatarUrl": "/images/avatar.png",
-            "verified": "true",
-            "name": "sth"
-          },
-          "nft": {
-            "name": "String",
-            "user": {
-              "name": "String",
-              "avatarUrl": "String",
-              "verified": "Boolean"
-            }
-          },
-          "type": "buy"
-        },
-      ]
-
+export default function Activity() {
     return (
         <>
             <Header />
+            <Hero text={"Activity"}/>
             <ActivityFilters filters={{ "sort": [{ "label": "Name (Ascending)", "value": 1 }, { "label": "Name (Descending)", "value": 2 }], "type": [{ "label": "Liked", "value": 3 }, { "label": "Bought", "value": 4 }] }} />
-            <ActivityList items={data}/>
+            <ActivityList items={dataActivity}/>
             <Footer />
         </>
     )
--- src/components/activity/ActivityListItem.jsx
+++ src/components/activity/ActivityListItem.jsx
@@ -7,9 +7,9 @@ import styles from "./ActivityListItem.module.scss"
 export default function ActivityItem({ user, created_at, nft, type = 'like' }) {
     return (
         <div className={classNames(styles["activity-item-container"])}>
-            <Avatar size={70} url={user.avatar.url} />
+            <Avatar size={70} url={user.avatar.url} verified={user.verified}/>
             <div className={classNames(styles["activity-item-para"])}>
-                <p><strong>{user.name}</strong> {type == 'like' ? 'liked' : 'bought'} <Link href="#" color="secondary">"{nft.name}"</Link> by <Link href="#" color="secondary">{nft.user.name}</Link></p>
+                <p><strong>{user.username}</strong> {type == 'like' ? 'liked' : 'bought'} <Link href="#" color="secondary">"{nft.name}"</Link> by <Link href="#" color="secondary">{nft.owner.username}</Link></p>
                 <p className={classNames(styles.date)}>{formatDistance(parseISO(created_at), Date.now())} ago</p>
             </div>
         </div>
