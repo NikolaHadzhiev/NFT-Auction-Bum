26c656f24ad6ce6473f7cfb35cc88fa485185f2d
Nikola Hadzhiev <nikihad2012@abv.bg>
2021-12-13 17:13:00 +0200

    feat: Implemented Activity page API calls

--- pages/activity/index.jsx
+++ pages/activity/index.jsx
@@ -4,14 +4,27 @@ import Hero from "../../src/components/hero/Hero.jsx"
 import ActivityFilters from "../../src/components/activity/ActivityFilters.jsx";
 import ActivityList from "../../src/components/activity/ActivityList.jsx";
 import Footer from "../../src/components/footer/Footer.jsx"
+import { useEffect, useState } from "react"
 
 export default function Activity() {
+    const [activity, setActivity] = useState()
+    const [activityFilters, setActivityFilters] = useState()
+
+    useEffect(async () => {
+        const response = await fetch(`${process.env.apiUrl}/activities`);
+            if (response.status == 200) {
+                const data = await response.json();
+                setActivity(data.activities);
+                setActivityFilters(data.filters);
+            }
+      }, [])
+
     return (
         <>
             <Header />
             <Hero text={"Activity"}/>
-            <ActivityFilters filters={{ "sort": [{ "label": "Name (Ascending)", "value": 1 }, { "label": "Name (Descending)", "value": 2 }], "type": [{ "label": "Liked", "value": 3 }, { "label": "Bought", "value": 4 }] }} />
-            <ActivityList items={dataActivity}/>
+            {activityFilters && <ActivityFilters filters={activityFilters} />}
+            {activity && <ActivityList items={activity} />}
             <Footer />
         </>
     )
